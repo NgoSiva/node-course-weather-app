5c6f8d35bca3c7473809a157d3b864fdebabff51
Siva Ngo <sivango4@gmail.com>
2020-04-26 22:02:11 +0200

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+            callback(
+                undefined,
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' +
+                body.current.feelslike + ' degress out. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
